18255d7e45117bd5062ddb93fdca07a482f4c1c8
Frederik Vantroys <frederik@agicon.be>
2025-07-02 15:09:28 +0200

    background bridge

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: BackGround,
+          backgroundImage: `url(${BackGround})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
